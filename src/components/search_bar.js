626ed4cc14a785bc4445ec7716a9cffa867d7258
isaacloft <isaacloft0906@gmail.com>
2018-09-23 18:14:48 +1000

    updates

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class SearchBar extends Component {
+export default class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+	}
+
 	render() {
-		return <input onChange={this.onInputChange} />;
+		return (
+			<div>
+				<input value={this.state.term} onChange={this.onInputChange.bind(this)} />
+			</div>
+		);
 	}
 
 	onInputChange(event) {
-		console.log(event.target.value);
+		this.setState({ term: event.target.value });
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY_YOUTUBE = 'AIzaSyBvuQWEnyly3-GRI5B3mO7mKk6UBGi-P9s';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-			hello
-		</div>
-	);
-};
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch(
+			{ key: API_KEY_YOUTUBE, term: 'zelda switch review' },
+			videos => {
+				this.setState({ videos });
+			}
+		);
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+}
 
 ReactDom.render(<App />, document.querySelector('.container'));
