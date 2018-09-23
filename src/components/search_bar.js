e77be587ff30a6f2b8f00b7e8a3f9646b966cf20
isaacloft <isaacloft0906@gmail.com>
2018-09-23 13:32:32 +1000

    updated components

---
+++ src/components/search_bar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+	render() {
+		return <input onChange={this.onInputChange} />;
+	}
+
+	onInputChange(event) {
+		console.log(event.target.value);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY_YOUTUBE = 'AIzaSyBvuQWEnyly3-GRI5B3mO7mKk6UBGi-P9s';
 
 const App = () => {
-	return <div>hello</div>;
-}
+	return (
+		<div>
+			<SearchBar />
+			hello
+		</div>
+	);
+};
 
 ReactDom.render(<App />, document.querySelector('.container'));
