ccf2a617a4ec2c7135dd7da2baa5a64f78d271cb
isaacloft <isaacloft0906@gmail.com>
2018-09-24 14:04:17 +1000

    live search search bar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -17,20 +18,33 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch(
-			{ key: API_KEY_YOUTUBE, term: 'zelda switch review' },
-			videos => {
-				this.setState({ videos: videos, selectedVideo: videos[0] });
-			}
-		);
+		this.videoSearch('Udemy');
+	}
+
+	videoSearch(term) {
+		YTSearch({ key: API_KEY_YOUTUBE, term: term }, videos => {
+			this.setState({ videos: videos, selectedVideo: videos[0] });
+		});
 	}
 
 	render() {
+
+		const videoSearch = _.debounce((term) => {
+			this.videoSearch(term);
+		}, 300);
+
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar
+					onSearchTermChange={videoSearch}
+				/>
 				<VideoDetail video={this.state.selectedVideo} />
-				<VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
+				<VideoList
+					onVideoSelect={selectedVideo =>
+						this.setState({ selectedVideo })
+					}
+					videos={this.state.videos}
+				/>
 			</div>
 		);
 	}
